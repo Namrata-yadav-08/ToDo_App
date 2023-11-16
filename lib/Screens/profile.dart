6d4e12cac1a8abd90d58cb4683d4649c2b434626
Namrata-yadav-08 <namratanyyadav@gmail.com>
2023-11-16 13:06:10 +0530

    updated profile

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -10,9 +10,83 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 19, 19, 21),
-      appBar: AppBar(elevation: 0),
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Profile',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  CircleAvatar(
+                    radius: 24,
+                  ),
+                  Text('Nammo'),
+                ],
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+              ),
+              Container(),
+            ],
+          ),
+          Text('settings'),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Text('settings'),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Text('Account'),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          ),
+          Container(
+            width: 90,
+            height: 30,
+            color: Colors.white,
+          )
+        ],
+      )),
     );
   }
 }
